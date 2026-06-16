057d800f7c176849f3de8240535dc37d1293b686
myu-kim <yuminkym5@gmail.com>
2026-06-16 23:45:09 +0900

    feat: add api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI
+from urllib import request
 
-from backend.app.schema import AnalysisRequest, AnalysisResponse
-from backend.app.service import AnalysisService
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncConnection
+
+from backend.app.service import project_service
+from backend.app.service import analysis_service
+from backend.app.schema.project import ProjectRequest, ProjectResponse
+from backend.app.schema.analysis import AnalysisRequest, AnalysisResponse
+from backend.app.db.connection import get_db_connection
 
 
 app = FastAPI()
 
-@app.post("/analysis", response_model=AnalysisResponse)
-async def request_analysis(request: AnalysisRequest) -> AnalysisResponse:
-    response = await AnalysisService.create_analysis(request)
+@app.post("/project/create", response_model=ProjectResponse)
+async def create_project(
+        project: ProjectRequest,
+        db: AsyncConnection = Depends(get_db_connection)
+) -> ProjectResponse:
+    project_name = project.name
+    response = await project_service.create_project(project_name, db)
+    return response
+
+@app.get("/project/{project_id}")
+async def get_project(
+        project_id: int,
+        db: AsyncConnection = Depends(get_db_connection)
+) -> ProjectResponse:
+    response = await project_service.get_project(project_id, db)
+    return response
+
+@app.get("/analysis/{analysis_id}")
+async def get_analysis(
+        analysis_id: int,
+    db: AsyncConnection = Depends(get_db_connection)
+) -> AnalysisResponse:
+    response = await analysis_service.get_analysis(analysis_id=analysis_id, connection=db)
     return response
